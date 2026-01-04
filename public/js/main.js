03cb418ba55af2625a26428bd28e0baac0b18959
NoOne0111 <189604366+NoOne0111@users.noreply.github.com>
2026-01-04 17:45:28 +0100

    Ajustement "Notre Histoire" 2

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,9 @@
 window.onload = function() {
     loadHeader();
     loadFooter();
+    var paypal = document.getElementById("paypal");
+    if (paypal) {
+        var paypalUrl = "https://www.paypal.com/donate/?hosted_button_id=VYR4JZ9CBN7XU";
+        paypal.setAttribute("href", paypalUrl);
+    }
 };
