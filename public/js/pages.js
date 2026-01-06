94d2a95914637690a4cf81ef279b1edd84e5e1fa
NoOne0111 <189604366+NoOne0111@users.noreply.github.com>
2026-01-06 14:03:43 +0100

    Ajustement "Notre Histoire" 10

--- public/js/pages.js
+++ public/js/pages.js
@@ -1,4 +1,19 @@
-var donationlink = document.getElementById("donation-home");
+var donationlink = document.getElementById("donation-home-header");
+if (donationlink) {
+    donationlink.setAttribute("href", donationUrl);
+}
+
+var donationlink = document.getElementById("donation-home-banner");
+if (donationlink) {
+    donationlink.setAttribute("href", donationUrl);
+}
+
+var donationlink = document.getElementById("donation-home-mission");
+if (donationlink) {
+    donationlink.setAttribute("href", donationUrl);
+}
+
+var donationlink = document.getElementById("donation-home-footer");
 if (donationlink) {
     donationlink.setAttribute("href", donationUrl);
 }
